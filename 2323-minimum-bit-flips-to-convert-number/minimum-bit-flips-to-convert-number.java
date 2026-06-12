f7ac8848f414f2201fe7c6319ab3bdd77e7891aa
Sanka Vamsi Akshay <sankavamsiakshay@gmail.com>
2026-06-12 10:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (33.32%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int cnt = 0;
+        while(start > 0 || goal > 0){
+            if((start % 2 == 0 && goal % 2 != 0) || (start % 2 != 0 && goal % 2 == 0)){
+                cnt += 1;
+            }
+            start /= 2;
+            goal /= 2;
+        }
+        return cnt;
+    }
+}
