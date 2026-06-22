cda71f260236307aded9587e13573838c0ecf493
Sanka Vamsi Akshay <sankavamsiakshay@gmail.com>
2026-06-22 13:07:17 +0530

    Time: 130 ms (35.84%) | Memory: 40.1 MB (6.65%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -1,21 +1,14 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        m = p = g = 0
+        garbageIdx = {"M" : 0, "P" : 0, "G" : 0}
         res = 0
         for idx, i in enumerate(garbage):
             for j in i:
                 res += 1
-                match j:
-                    case "M": m = idx
-                    case "P": p = idx
-                    case "G": g = idx
-                    case _: pass
+                garbageIdx[j] = idx
         for i in range(1, len(travel)):
             travel[i] += travel[i - 1]
-        if m:
-            res += travel[m - 1]
-        if p:
-            res += travel[p - 1]
-        if g:
-            res += travel[g - 1]
+        for i in garbageIdx:
+            if garbageIdx[i] != 0:
+                res += travel[garbageIdx[i] - 1]
         return res
