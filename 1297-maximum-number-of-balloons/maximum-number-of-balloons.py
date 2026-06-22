8b5fea0da7204a8233fa28bb2a046f7b46ce266d
Sanka Vamsi Akshay <sankavamsiakshay@gmail.com>
2026-06-22 09:28:37 +0530

    Time: 3 ms (61.87%) | Memory: 19.3 MB (75.19%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,10 +1,13 @@
 from collections import Counter
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        text = Counter(text)
-        b = text.get('b', 0)
-        a = text.get('a', 0)
-        l = text.get('l', 0) // 2
-        o = text.get('o', 0) // 2
-        n = text.get('n', 0)
-        return min(b, a, l, o, n)
+        b = a = l = o = n = 0
+        for i in text:
+            match i:
+                case 'b': b += 1
+                case 'a': a += 1
+                case 'l': l += 1
+                case 'o': o += 1
+                case 'n': n += 1
+                case _: pass
+        return min(b, a, l // 2, o // 2, n)
