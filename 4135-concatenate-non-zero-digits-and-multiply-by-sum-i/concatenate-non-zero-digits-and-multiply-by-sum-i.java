4a2cf0ac7ef131d8a62e374392f714445c44ed96
Sanka Vamsi Akshay <sankavamsiakshay@gmail.com>
2026-07-07 09:29:43 +0530

    Time: 1 ms (99.85%) | Memory: 42.7 MB (34.98%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -3,13 +3,13 @@ class Solution {
         int sum = 0;
         int x = 0;
         int val;
-        int p = 0;
+        int p = 1;
         while(n > 0){
             val = n % 10;
             if(val != 0){
                 sum += val;
-                x += (Math.pow(10, p) * val);
-                p += 1;
+                x += (p * val);
+                p *= 10;
             }
             n /= 10;
         }
