91905e8dc3dd5214db36314c5462f14315c11c66
Sanka Vamsi Akshay <sankavamsiakshay@gmail.com>
2026-07-12 20:16:09 +0530

    Time: 29 ms (95.39%) | Memory: 37.6 MB (39.04%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,15 +1,7 @@
 import heapq
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        dup = arr[:]
-        heapq.heapify(dup)
         rank = {}
-        r = 1
-        while dup:
-            v = heapq.heappop(dup)
-            if v not in rank:
-                rank[v] = r
-                r += 1
-        for i, j in enumerate(arr):
-            arr[i] = rank.get(j)
-        return arr
+        for r, num in enumerate(sorted(set(arr)), 1):
+            rank[num] = r
+        return [rank[num] for num in arr]
